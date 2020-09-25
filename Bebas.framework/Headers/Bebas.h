ebc042e7ef6c2baea412f3e172298f984f98a426
Asep Mulyana <asepmoels@gmail.com>
2020-09-25 14:20:20 +0700

    First Version 1.0

---
+++ Bebas.framework/Headers/Bebas.h
@@ -0,0 +1,19 @@
+//
+//  Bebas.h
+//  Bebas
+//
+//  Created by Asep Mulyana on 25/09/20.
+//  Copyright © 2020 Asep Mulyana. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Bebas.
+FOUNDATION_EXPORT double BebasVersionNumber;
+
+//! Project version string for Bebas.
+FOUNDATION_EXPORT const unsigned char BebasVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bebas/PublicHeader.h>
+
+
